63a600dea6cbd1fe99cb4c41fbf989559674b652
Sunil Gawai <sunilgawai7397@gmail.com>
2023-07-17 01:58:23 +0530

    Added Update and Delete Controllers.

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -59,6 +59,82 @@ class CustomerController {
             return next(error);
         }
     }
+
+    static async view(req: Request, res: Response, next: NextFunction) {
+        if (!req.params.id) {
+            return next(new Error('Invalid params'));
+        }
+        try {
+            await database.customer.findUnique({
+                where: {
+                    id: req.params.id
+                }
+            }).then((customer) => {
+                res.status(200).json(customer);
+            }).catch((err) => {
+                return next(err);
+            })
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    static async update(req: Request, res: Response, next: NextFunction) {
+        // updating customers.
+        const { error } = CustomerValidator.store_request(req.body);
+
+        if(error) {
+            return next(error);
+        }
+
+        try {
+            await database.customer.update({
+                where: {
+                    id: req.params.id
+                },
+                data: {
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    phone: req.body.phone,
+                    email: req.body.email,
+                    countryId: req.body.country,
+                    stateId: req.body.state,
+                    cityId: req.body.city,
+                    kyc: req.body.kyc,
+                    address: req.body.address
+                },
+                include: {
+                    Shop: true,
+                    kyc: true
+                }
+            }).then((customer) => {
+                res.status(200).send(customer);
+            }).catch((err) => {
+                return next(err);
+            })
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    static async delete(req: Request, res: Response, next: NextFunction) {
+        if (!req.params.id) {
+            return next(new Error('Invalid params.'));
+        }
+        try {
+            await database.customer.delete({
+                where: {
+                    id: req.params.id
+                }
+            }).then((customer) => {
+                res.status(200).json(customer);
+            }).catch((err) => {
+                return next(err);
+            })
+        } catch (error) {
+            return next(error);
+        }
+    }
 }
 
 export default CustomerController;
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -8,6 +8,10 @@ customerRouter.post('/customer', auth, CustomerController.store);
 
 customerRouter.get('/customer', auth, CustomerController.get);
 
+customerRouter.get('/customer/:id', auth, CustomerController.view);
+
+customerRouter.delete('/customer/:id', auth, CustomerController.delete);
+
 
 
 export default customerRouter;
